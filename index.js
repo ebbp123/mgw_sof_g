9380bb39265f4179dbdcf447fa88c99f34249be0
Esme Bolanos <esme.bolanos@cerner.com>
2023-05-22 11:50:25 -0500

    Add patch operation

--- index.js
+++ index.js
@@ -21,6 +21,31 @@ async function getPatient() {
     return await response.json()
 }
 
+const patchTestAddress = `[
+    {
+      "path": "/address/0/id",
+      "op": "test",
+      "value": "CI-25151561-0"
+    }
+]`;
+
+// Only makes a test operation
+async function patchPatient() {
+    let response = await fetch(fhirUrl + '/Patient/' + token_data.patient, {
+        method: 'PATCH',
+        headers: {
+            'Accept': 'application/fhir+json',
+            'Content-Type': 'application/json-patch+json',
+            'Authorization': `Bearer ${token_data.access_token}`,
+            'If-Match': 'W/"34"',
+            "Cerner-Deployment-Config": 'ehr-sandbox'
+        },
+        body: patchTestAddress
+    })
+
+    return await response.status()
+}
+
 getPatient().then((data) => {
     console.log(data)
     document.getElementById('mgw-data-url').textContent = fhirUrl + '/Patient/' + token_data.patient
@@ -29,3 +54,15 @@ getPatient().then((data) => {
     debugger
     console.log('error fetching patient data')
 })
+
+
+patchPatient().then((data) => {
+    console.log(data)
+    document.getElementById('post-action').textContent = 'Patched Patient'
+    document.getElementById('post-data-status').textContent= data
+}).catch((err) => {
+    debugger
+    console.log('error patching patient')
+})
+
+
